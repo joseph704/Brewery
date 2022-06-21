a91a2b24c441fad8e2fe8c4148d801d13cdeee29
차요셉 <jscha@raoncorp.com>
2022-06-21 21:59:11 +0900

    Punk API로 부터 데이터 fetch

--- Brewery/BeerDetailViewController.swift
+++ Brewery/BeerDetailViewController.swift
@@ -39,7 +39,7 @@ extension BeerDetailViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 3:
-            return beer?.foodParing?.count ?? 0
+            return beer?.foodPairing?.count ?? 0
         default:
             return 1
         }
@@ -54,7 +54,7 @@ extension BeerDetailViewController {
         case 2:
             return "Brewers Tips"
         case 3:
-            let isFoodParingEmpty = beer?.foodParing?.isEmpty ?? true
+            let isFoodParingEmpty = beer?.foodPairing?.isEmpty ?? true
             return isFoodParingEmpty ? nil : "Food Parsing"
         default:
             return nil
@@ -79,7 +79,7 @@ extension BeerDetailViewController {
             cell.textLabel?.text = beer?.brewersTips ?? "팁 없는 맥주"
             return cell
         case 3:
-            cell.textLabel?.text = beer?.foodParing?[indexPath.row] ?? ""
+            cell.textLabel?.text = beer?.foodPairing?[indexPath.row] ?? ""
             return cell
         default:
             return cell
--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class BeerListViewController: UITableViewController {
-    var beerList = [Beer]()
+    var beerList: [Beer] = [Beer]()
+    var currentPage: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class BeerListViewController: UITableViewController {
         //UITableView 설정
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
+        
+        fetchBeer(of: currentPage
+        )
     }
 }
 
@@ -45,5 +49,52 @@ extension BeerListViewController {
         
         detailViewController.beer = selectedBeer
         self.show(detailViewController, sender: nil)
+    }
+}
+
+//Data Fetching
+private extension BeerListViewController {
+    func fetchBeer(of page: Int) {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page-\(page)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data) else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            switch response.statusCode {
+            case 200...299: // 성공
+                self.beerList += beers
+                self.currentPage += 1
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case 400...499: // 클라이언트 에러
+                print("""
+                ERROR: Client ERROR \(response.statusCode)
+                Response: \(response)
+            """)
+            case 500...599: // 서버 에러
+                print("""
+                ERROR: Server ERROR \(response.statusCode)
+                Response: \(response)
+            """)
+            default:
+                print("""
+                ERROR: \(response.statusCode)
+                Response: \(response)
+            """)
+            }
+        }
+        dataTask.resume()
     }
 }
