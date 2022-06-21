38795d507768b7880b603ea8c513ec1bbde65489
차요셉 <jscha@raoncorp.com>
2022-06-21 21:31:41 +0900

    기본 UI 구성

---
+++ Brewery/BeerListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BeerListViewController.swift
+//  Brewery
+//
+//  Created by Joseph Cha on 2022/06/21.
+//
+
+import UIKit
+
+class BeerListViewController: UITableViewController {
+    var beerList = [Beer]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UINavigationBar
+        title = "패캠브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //UITableView 설정
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
+        tableView.rowHeight = 150
+    }
+}
+
+//UItableView DataSource, Delegate
+extension BeerListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
+        
+        let beer = beerList[indexPath.row]
+        cell.configure(with: beer)
+        
+        return cell
+                
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBeer = beerList[indexPath.row]
+        let detailViewController = BeerDetailViewController()
+        
+        detailViewController.beer = selectedBeer
+        self.show(detailViewController, sender: nil)
+    }
+}
